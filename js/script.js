a67ddb13adc4e8bd041c863c0c348e5511cfa514
KashyapRayas <kashyap.rayas@gmail.com>
2023-04-13 00:09:47 +0530

    cursor disappears on mobile

--- js/script.js
+++ js/script.js
@@ -498,6 +498,17 @@
     })
     }
 
+    //if website on mobile, no cursor
+    let details = navigator.userAgent;
+
+    let regexp = /android|iphone|kindle|ipad/i;
+    
+    let isMobileDevice = regexp.test(details);
+    
+    if (isMobileDevice) {
+        document.querySelector(".cursor").style.display = 'none'
+    }
+
     
 
     
