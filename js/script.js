ccc89ff4cd4cbe745d9e6952d4a650d91197827a
KashyapRayas <kashyap.rayas@gmail.com>
2023-04-19 15:10:11 +0530

    redownloaded

--- js/script.js
+++ js/script.js
@@ -243,7 +243,6 @@
         }, '-=200');
         logo.onmouseover = logo_anime.play
     }
-
     
     function loader_logo_animation() {
 
@@ -321,13 +320,13 @@
         .add({
             targets: '#loading .page2',
             keyframes: [
-                {translateY: `-${window.screen.height}`, duration: 500}
+                {height: 0, duration: 500}
             ]
         }, '-=200')
         .add({
             targets: '#loading .page3',
             keyframes: [
-                {translateY: `-${window.screen.height}`, duration: 400}
+                {height: 0, duration: 400}
             ]
         }, '-=300')
     }
@@ -435,7 +434,7 @@
             window.setTimeout(()=>{
                 project_page.style.opacity = 1
             }, 300)
-            })
+        })
     })
 
 //exiting project page
@@ -463,6 +462,52 @@
             }, 400)
         })
     })
+
+    const $bigBall = document.querySelector('.cursor__ball--big');
+    const $smallBall = document.querySelector('.cursor__ball--small');
+    const $hoverables = document.querySelectorAll('.hoverable');
+
+    // Listeners
+    document.body.addEventListener('mousemove', onMouseMove);
+    for (let i = 0; i < $hoverables.length; i++) {
+    $hoverables[i].addEventListener('mouseenter', onMouseHover);
+    $hoverables[i].addEventListener('mouseleave', onMouseHoverOut);
+    }
+
+    // Move the cursor
+    function onMouseMove(e) {
+    TweenMax.to($bigBall, .4, {
+        x: e.pageX - 15,
+        y: e.pageY - 15
+    })
+    TweenMax.to($smallBall, .1, {
+        x: e.pageX - 5,
+        y: e.pageY - 7
+    })
+    }
+
+    // Hover an element
+    function onMouseHover() {
+    TweenMax.to($bigBall, .3, {
+        scale: 4
+    })
+    }
+    function onMouseHoverOut() {
+    TweenMax.to($bigBall, .3, {
+        scale: 1
+    })
+    }
+
+    //if website on mobile, no cursor
+    let details = navigator.userAgent;
+
+    let regexp = /android|iphone|kindle|ipad/i;
+    
+    let isMobileDevice = regexp.test(details);
+    
+    if (isMobileDevice) {
+        document.querySelector(".cursor").style.display = 'none'
+    }
 
     
 
